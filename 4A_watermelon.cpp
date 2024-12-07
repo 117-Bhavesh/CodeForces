c877ec9c8281407447fdeb3b66a70791f49411ca
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-12-07 19:13:17 +0530

    #4A Watermelon

---
+++ 4A_watermelon.cpp
@@ -0,0 +1,19 @@
+//4A - Watermelon 117-Bhavesh Accepted
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    if (w % 2 == 0 && w > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
